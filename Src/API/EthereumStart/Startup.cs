c87db8202a4dec8376fc98e8124a118dba2d98b1
abdul.basid <abdul.basid@dh.com>
2018-02-11 14:23:00 +0530

    Source for api

---
+++ Src/API/EthereumStart/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using EthereumStart.Models;
+using EthereumStart.Services;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace EthereumStart
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            //services.Configure<EthereumSettings>(Configuration);
+            services.AddScoped<IEthereumService, BasicEthereumService>();
+
+            services.Configure<EthereumSettings>(myOptions =>
+            {
+                //myOptions.EhtereumAccount = "0xEE1418af155D6692322C2f7DaaC05fb3d3a5FD47";
+                myOptions.EhtereumAccount = "0x5A5AA2714b31CFE3FD626B479a466Cf63EB90248";
+                myOptions.EhtereumPassword = "123abc123A";
+                myOptions.StorageAccount = "hackathondiag646";
+                myOptions.StorageKey = "u8jVpVxu5pXPpYY97SFNejzS5yolIFoDO3irONny+rtPohyyL24Bkuuvaicro+WDBKOO84WPq13I0CiZaI4iLA==";
+            });
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseMvc();
+
+
+
+        }
+    }
+}
