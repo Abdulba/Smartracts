b8421f5ea6db22a8b75a8553fc1ae1185e48b80f
abdul.basid <abdul.basid@dh.com>
2018-02-12 17:23:28 +0530

    Api ui calls

--- Src/UI/scripts/Smart.js
+++ Src/UI/scripts/Smart.js
@@ -22,32 +22,130 @@
 
  }
 
- function GetAccountBalanceHttp(callback)
+function GetAccountBalance()
  {
      var xhttp = new XMLHttpRequest();
      var getBalanceUrl = "http://smartractsapi.azurewebsites.net/api/EthereumTest/getBalance/0x4DccD0F0f1d4C11b93a1D027894892652122fEcE";
 
-     //xhttp.addEventListener('load', callback);
-     xhttp.addEventListener('error', () => console.log("Request to " + getBalanceUrl + " failed"));
      xhttp.onreadystatechange = function () {
-         if (xhttp.readyState == 4 && xhttp.status == 200) {
-             callback(xhttp);
+         if (xhttp.readyState == 4) {
+             if (xhttp.status === 200) {
+                 var response = JSON.parse(xhttp.responseText);
+                 alert(response);
+             } else {
+                 alert(xhttp.status);
+                 alert("error");
+             }
          }
      }
-     xhttp.open("GET", getBalanceUrl, true);
+
+     xhttp.open("GET", getBalanceUrl, false);
      xhttp.setRequestHeader("Content-type", "application/json");
      xhttp.send();
-     //alert(xhttp.responseText);
-
-     //alert(JSON.parse(xhttp.responseText));
- }
-
- function GetAccountBalance()
- {
-     GetAccountBalanceHttp(function (xhttp) {
-         debugger;
-         alert(xhttp.responseText);
-     })
  }
 
 
+function SetConcurrency(userId, count) {
+    var xhttp = new XMLHttpRequest();
+    var setConUrl = "http://smartractsapi.azurewebsites.net/api/EthereumTest/exeContract/setConcurrency/" + count + "/" + userId;
+
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4) {
+            if (xhttp.status === 200) {
+                var response = JSON.parse(xhttp.responseText);
+                alert(response);
+            } else {
+                alert(xhttp.status);
+                alert("error");
+            }
+        }
+    }
+
+    xhttp.open("GET", setConUrl, false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+}
+
+function login(userId) {
+    var xhttp = new XMLHttpRequest();
+    var loginUrl = "http://smartractsapi.azurewebsites.net/api/EthereumTest/exeContract/login/" + userId;
+
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4) {
+            if (xhttp.status === 200) {
+                var response = JSON.parse(xhttp.responseText);
+                alert(response);
+            } else {
+                alert(xhttp.status);
+                alert("error");
+            }
+        }
+    }
+
+    xhttp.open("GET", loginUrl, false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+}
+
+function logout(userId) {
+    var xhttp = new XMLHttpRequest();
+    var logoutUrl = "http://smartractsapi.azurewebsites.net/api/EthereumTest/exeContract/logout/" + userId;
+
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4) {
+            if (xhttp.status === 200) {
+                var response = JSON.parse(xhttp.responseText);
+                alert(response);
+            } else {
+                alert(xhttp.status);
+                alert("error");
+            }
+        }
+    }
+
+    xhttp.open("GET", logoutUrl, false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+}
+
+function GetConcurrentBalance(userId) {
+    var xhttp = new XMLHttpRequest();
+    var getConBalanceUrl = "http://smartractsapi.azurewebsites.net/api/EthereumTest/exeContract/GetConcurrentUserBalance/" + userId;
+
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4) {
+            if (xhttp.status === 200) {
+                var response = JSON.parse(xhttp.responseText);
+                alert(response);
+            } else {
+                alert(xhttp.status);
+                alert("error");
+            }
+        }
+    }
+
+    xhttp.open("GET", getConBalanceUrl, false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+}
+
+function TransferEthers(userId, accountTo, ethers) {
+    var xhttp = new XMLHttpRequest();
+    var transferUrl = "http://smartractsapi.azurewebsites.net/api/EthereumTest/exeContract/TransferEther/" + userId + "/" + accountTo + "/" + ethers;
+
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4) {
+            if (xhttp.status === 200) {
+                var response = JSON.parse(xhttp.responseText);
+                alert(response);
+            } else {
+                alert(xhttp.status);
+                alert("error");
+            }
+        }
+    }
+
+    xhttp.open("GET", transferUrl, false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+}
--- Src/UI/scripts/app.js
+++ Src/UI/scripts/app.js
@@ -1,10 +1,3 @@
-//web3 = new Web3(new Web3.providers.HttpProvider("http://52.187.176.233:8000"));
-//abi = JSON.parse('[ { "constant": true, "inputs": [ { "name": "", "type": "address" } ], "name": "currentCount", "outputs": [ { "name": "", "type": "uint256", "value": "0" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "accountId", "type": "address" } ], "name": "login", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "address" } ], "name": "maxCount", "outputs": [ { "name": "", "type": "uint256", "value": "2" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "accountId", "type": "address" }, { "name": "count", "type": "uint256" } ], "name": "setConcurrency", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "accountId", "type": "address" } ], "name": "getConcurrencyBalance", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "accountId", "type": "address" } ], "name": "logout", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "currentCount", "type": "uint256" }, { "indexed": true, "name": "user", "type": "address" } ], "name": "Transaction", "type": "event" } ]')
-//conContract = web3.eth.contract(abi);
-
-//contractInstance = conContract.at('0xB9162DCD3B4Dd64B34Dfc48276e314469c09D022');
-//account = "0x5A5AA2714b31CFE3FD626B479a466Cf63EB90248";
-
 window.App = {
  setStatus: function(message) {
     alert(message);
@@ -12,23 +5,24 @@ window.App = {
 
  login: function() {
     var self = this;
-    //contractInstance.login(account, {from: account});
-    //self.refreshBalance();
-    GetAccountBalance();
+    login('0x4DccD0F0f1d4C11b93a1D027894892652122fEcE');
     window.location.href = "welcome.html";
   },
 
   logout: function() {
     var self = this;
-    //contractInstance.logout(account, {from: account});
-    //self.refreshBalance();
-    GetAccountBalance();
+    logout('0x4DccD0F0f1d4C11b93a1D027894892652122fEcE');
     window.location.href = "login.html";
+ },
+
+  payEthers: function () {
+      var self = this;
+      TransferEthers('0x5A5AA2714b31CFE3FD626B479a466Cf63EB90248', '0x4DccD0F0f1d4C11b93a1D027894892652122fEcE', 10);
+      window.location.href = "login.html";
   },
 
   refreshBalance: function() {
     var self = this;
-    //var  value = contractInstance.getConcurrencyBalance.call(account, {from: account});
     alert(value.valueOf());
   }
 };
